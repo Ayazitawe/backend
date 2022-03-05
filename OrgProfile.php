5c17e342b3a8b750caa5ea55067d1f6ac3f7dfcb
Ayazitawe <71343143+Ayazitawe@users.noreply.github.com>
2022-03-05 17:39:11 +0200

    Add files via upload

---
+++ OrgProfile.php
@@ -0,0 +1,21 @@
+<?php
+include('db.php');
+// Creating connection.
+ $conn= mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+ 
+ // Getting the received JSON into $json variable.
+ $json = file_get_contents('php://input');
+  // decoding the received JSON and store into $obj variable.
+ $decodedData= json_decode($json,true);
+  $UserEmail = $decodedData['Email'];
+  $SQL = "SELECT * FROM org WHERE orgEmail = '$UserEmail'";
+    $exeSQL = mysqli_query($conn, $SQL);
+    $arrayu = mysqli_fetch_array($exeSQL);
+    $response[] = array("Message0" => $arrayu['orgName'],
+       "Message1" => $arrayu['orgPhone'],
+     "Message2" => $arrayu['orgCity']);
+	
+echo json_encode($response);
+  mysqli_close($conn);
+?>
+ 
