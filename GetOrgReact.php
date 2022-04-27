972890900bbf6263f03955f12881ce2a0b4da365
Ayazitawe <71343143+Ayazitawe@users.noreply.github.com>
2022-04-27 17:36:23 +0300

    Add files via upload

---
+++ GetOrgReact.php
@@ -0,0 +1,31 @@
+<?php
+include('db.php');
+// Creating connection.
+ $conn= mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+
+$json = file_get_contents('php://input');
+$decodedData= json_decode($json,true);
+// Creating SQL command to fetch all records from Table.
+$sql = "SELECT * FROM org where 1";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows >0) {
+ 
+ 
+ while($row[] = $result->fetch_assoc()) {
+ 
+ $response = $row;
+ 
+ $json = json_encode($response);
+ 
+ }
+ 
+} else {
+	$response="No Results Found";
+ $json = json_encode($response);
+}
+ echo $json;
+$conn->close();
+?>
+ 
